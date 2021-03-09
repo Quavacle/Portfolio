82b5481ddaa6799129952d4de38b88cd074ed00c
Bronson Myers <51137401+Quavacle@users.noreply.github.com>
2021-03-09 14:56:16 -0700

    Added sastang to projects

--- folio.js
+++ folio.js
@@ -1,44 +1,45 @@
-const panels = document.querySelectorAll('.header')
-const secret = document.querySelector('.secret')
-const leftButton = document.querySelector('.left')
-const rightButton = document.querySelector('.right')
-const projectsContainer = document.querySelector('.projects')
-let currentProject = 0
-const projects = ['asl.html', 'owfa.html', 'habit.html']
+const panels = document.querySelectorAll('.header');
+const secret = document.querySelector('.secret');
+const leftButton = document.querySelector('.left');
+const rightButton = document.querySelector('.right');
+const projectsContainer = document.querySelector('.projects');
+let currentProject = 0;
+const projects = ['satsang.html', 'asl.html', 'owfa.html'];
 
 function togglePanel() {
-  const currentlyActive = document.querySelector('.active') || null
+  const currentlyActive = document.querySelector('.active') || null;
   if (currentlyActive) {
-    currentlyActive.classList.remove('active')
+    currentlyActive.classList.remove('active');
   }
   if (currentlyActive === this.parentNode) {
-    this.parentNode.classList.remove('active')
+    this.parentNode.classList.remove('active');
   } else {
-    this.parentNode.classList.toggle('active')
+    this.parentNode.classList.toggle('active');
   }
 }
 
 function changeProject(forward = true) {
+  console.log('Change project hit');
   if (forward) {
     if (currentProject === projects.length - 1) {
-      currentProject = 0
+      currentProject = 0;
     } else {
-      currentProject++
+      currentProject++;
     }
   } else {
     if (currentProject === 0) {
-      currentProject = projects.length - 1
+      currentProject = projects.length - 1;
     } else {
-      currentProject--
+      currentProject--;
     }
   }
   projectsContainer.setAttribute(
     'data',
     './projects/' + projects[currentProject]
-  )
+  );
 }
 
-panels.forEach((panel) => panel.addEventListener('click', togglePanel))
+panels.forEach((panel) => panel.addEventListener('click', togglePanel));
 
-leftButton.addEventListener('click', () => changeProject(false))
-rightButton.addEventListener('click', () => changeProject())
+leftButton.addEventListener('click', () => changeProject(false));
+rightButton.addEventListener('click', () => changeProject());
